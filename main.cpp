1546e72465811b775361b4339093178b1d79b5af
hadi-naeisseh <hadinaeisseh17@gmail.com>
2024-09-04 14:30:59 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <curl/curl.h>
 #include <json/json.h>
 
-// Function to fetch data from a URL
+// Function that fetches data from a URL
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
